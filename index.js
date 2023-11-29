da654a8b951d199cbf23bfdf1246d0c89b3c18ec
060-NAVEENKUMARM <145565904+Appuraj2304@users.noreply.github.com>
2023-11-29 17:04:53 +0530

    update

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const { Sequelize, DataTypes } = require('sequelize');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('port', process.env.PORT || 5000);
+
+app.get('/', (req, res) => {
+  res.send('Hello, Node.js!');
+});
+
+app.listen(app.get('port'), function () {
+    console.log("Node app is running at localhost:" + app.get('port'));
+  });
