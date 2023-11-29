4cf99870684aa70d4995ba85e58bce636cee52f5
060-NAVEENKUMARM <145565904+Appuraj2304@users.noreply.github.com>
2023-11-29 17:15:29 +0530

    update

--- index.js
+++ index.js
@@ -9,8 +9,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('port', process.env.PORT || 5000);
 
-app.get('/', (req, res) => {
-  res.send('Hello, Node.js!');
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
+app.get("/fitzz", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "fitzz.html"));
+});
+
+app.get("/gear", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "gear.html"));
+});
+
+app.get("/ridepopup", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "ridepopup.html"));
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "register.html"));
+});
+
+app.get("/soul", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "soul.html"));
+});
+
+app.get("/spirit", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "spirit.html"));
+});
+
+app.get("/cheackout", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "cheackout.html"));
+});
+
+app.get("/bot", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "bot.html"));
 });
 
 app.listen(app.get('port'), function () {
